66fd75897d5211844d029f2090dc929914e6825f
Justin Huang <justin.2260@gmail.com>
2022-02-11 17:29:12 -0500

    remove references to cookie parser

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 // db connection
@@ -15,7 +14,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter(db));
