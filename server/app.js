2cca778c63d1cbdea3cc44601efc87fdbc0caf14
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-10 22:54:37 -0500

    Refactor user routes

--- server/app.js
+++ server/app.js
@@ -17,6 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter());
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require('express').Router();
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = (db) => {
+  
+  /* GET users listing. */
+  router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+  });
 
-module.exports = router;
+  return router;
+}
