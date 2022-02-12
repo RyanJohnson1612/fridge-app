c7761fefebd5dc8be2e5522dedc5dbb6583619f8
Bita <bita.janzadeh@hotmail.com>
2022-02-11 22:06:25 -0500

    Add 'close' and 'edit' buttons to ShoppingListItems component

--- client/src/ShoppingList/ShoppingListItem.js
+++ client/src/ShoppingList/ShoppingListItem.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import ShoppingListForm from "./ShoppingListForm";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
 
 function ShoppingListItem() {
   const [edit, setEdit] = useState({
@@ -17,6 +19,11 @@ function ShoppingListItem() {
       <div key={item.id} onClick={() => completeItem(item.id)}>
         {item.text}
       </div>
+
+      <div className="icons">
+        <RiCloseCircleLine />
+        <TiEdit />
+      </div>
     </div>
   ));
 }
@@ -26,5 +33,3 @@ export default ShoppingListItem;
 {
   /* Function to check if item has been purchased or not */
 }
-
-Map through items in ShoppingListItem component, WIP
