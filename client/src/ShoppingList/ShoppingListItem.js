77803e9d56a2296f07587720785fa3bfa519ceaa
Bita <bita.janzadeh@hotmail.com>
2022-02-11 21:17:53 -0500

    Initial setup of ShoppingListItem component

---
+++ client/src/ShoppingList/ShoppingListItem.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import ShoppingListForm from "./ShoppingListForm";
+
+function ShoppingListItem() {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+
+  return <div></div>;
+}
+
+export default ShoppingListItem;
