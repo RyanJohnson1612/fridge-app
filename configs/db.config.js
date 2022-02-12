7be155ae42f72536f5e48dc00d6f03af73e3a163
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-12 02:23:33 -0500

    Setup Jest for testing api server

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,15 +1,15 @@
 // Database connections
 const { Pool } = require('pg');
 
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT} = process.env;
+const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT, TEST_DATABASE} = process.env;
 
 const pool = new Pool({
-	user: DB_USER,
-	host: DB_HOST,
-	password: DB_PASSWORD,
-	port: DB_PORT,
-	database: DB_DATABASE,
-})
+  user: DB_USER,
+  host: DB_HOST,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  database: process.env.NODE_ENV !== 'test' ? DB_DATABASE : TEST_DATABASE
+});
 
 pool.connect().then(() => {
 	console.log("Database connection established.")
