8ed0bdbbd54be7ee0734e9be899ace68ea18013c
Bita <bita.janzadeh@hotmail.com>
2022-02-12 13:58:42 -0500

    Add toggle and responsive attributes to Navbar

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="myTeal" variant="dark" fixed="top">
+      <Navbar bg="myTeal" variant="dark" fixed="top" expand="sm">
         <Navbar.Brand href="/">FridgeApp</Navbar.Brand>
+        <Navbar.Toggle />
+        <NavbarCollapse>
+          <Nav>
+            <Nav.Link href="/"> MyFridge</Nav.Link>
+            <Nav.Link href="/"> Grocery List</Nav.Link>
+            <Nav.Link href="/"> Meal Ideas</Nav.Link>
+          </Nav>
+        </NavbarCollapse>
       </Navbar>
+
       <div className="content">Placeholder content...</div>
     </div>
   );
