3de93963aa0662f9b604e5cd644a249eed53a247
Bita <bita.janzadeh@hotmail.com>
2022-02-11 21:09:56 -0500

    Create addItem function in ShoppingList component which adds items to shopping list

--- client/src/ShoppingList/ShoppingList.js
+++ client/src/ShoppingList/ShoppingList.js
@@ -4,10 +4,17 @@ import ShoppingListForm from "./ShoppingListForm";
 function ShoppingList() {
   const [items, setItems] = useState([]);
 
+  //Function to add items to shopping list, will be passed to ShoppingListForm using onSubmit
+  const addItem = (item) => {
+    const newItems = [item, ...items];
+    setItems(newItems);
+    console.log(...items);
+  };
+
   return (
     <div>
       <h1> My Grocery List</h1>
-      <ShoppingListForm />
+      <ShoppingListForm onSubmit={addItem} />
     </div>
   );
 }
