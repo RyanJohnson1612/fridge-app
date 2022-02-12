fae59daa0536e8d763a3d112d8f4ab58a0182e20
Justin Huang <justin.2260@gmail.com>
2022-02-12 16:55:07 -0500

    initial formatting

--- client/src/components/FridgeItem/FridgeItem.jsx
+++ client/src/components/FridgeItem/FridgeItem.jsx
@@ -2,18 +2,57 @@ import React from 'react';
 import './FridgeItem.scss';
 
 const FridgeItem = (props) => {
+
+  const daysAgo = (days) => {
+    if (days === 0) {
+      return "Today";
+    } else if (days === 1) {
+      return `${days} day ago`;
+    } else {
+      return `${days} days ago`;
+    }
+  };
+
   return (
-    <>
+    <div className='body'>
       <img src={props.image} className='image' />
-      <div className='properties'>
-        <p>{props.name}</p>
-        <p>Category: {props.category}</p>
-        <p>Date Purchased: {props.dateStored}</p>
-        { props.expiry ? <p>Expiry Date: {props.expiry}</p> : "" }
-        { props.expiry ? <p>Time Remaining: {props.expiry - props.dateStored}</p> : ""}
-        <p>Notes: {props.notes}</p>
-      </div>
-    </>
+      <table className='properties'>
+        <tbody>
+          <tr>
+            <td>Food Item: </td>
+            <td>{props.name}</td>
+          </tr>
+          <tr>
+            <td>Category: </td>
+            <td>{props.category}</td>
+          </tr>
+          <tr>
+            <td>Date Purchased: </td>
+            <td>{props.dateStored}</td>
+          </tr>
+          <tr>
+            <td>Been in the Fridge since how long: </td>
+            <td>{daysAgo(props.storedSince)}</td>
+          </tr>
+          { props.expiry ?
+            <tr>
+              <td>Expiry Date: </td>
+              <td>{props.expiry}</td>
+            </tr>
+          : "" }
+          { props.expiry ?
+            <tr>
+              <td>Time Remaining: </td>
+              <td>{props.expireIn} Days</td>
+            </tr>
+          : ""}
+          <tr>
+            <td>Notes: </td>
+            <td>{props.notes}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
   );
 }
