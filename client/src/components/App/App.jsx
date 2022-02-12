239f688c05028e60bb5098f04a67a68c6f696294
Justin Huang <justin.2260@gmail.com>
2022-02-12 13:13:59 -0500

    test db query retrieval to send to FridgeItem component

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import FridgeItem from '../FridgeItem/FridgeItem';
 import './App.scss';
+import axios from 'axios'
 
 function App() {
+
+  const [food, setFood] = useState({});
+
+  useEffect(() => {
+    axios.get(`http://localhost:8080/food/6`)
+      .then((results) => {
+        const food = results.data[0]
+        console.log(food);
+        setFood(food);
+      }).catch(error => console.log(`Error: ${error.message}`));
+  }, []);
+
   return (
     <div className="App">
-      Fridge App (Name WIP)
+      <p>Fridge App (Name WIP)</p>
+      <FridgeItem
+        key={food.id}
+        name={food.name}
+        dateStored={food.date_stored}
+        expiry={food.expiry}
+        category={food.category}
+        image={food.image_url}
+        notes={food.notes}
+      />
     </div>
   );
 }
