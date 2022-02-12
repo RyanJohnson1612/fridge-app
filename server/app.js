97e499e649023f3d7cea84cc6b83fb58d5eecebb
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-12 17:21:35 -0500

    Create post route for creating user

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const bodyParser = require("body-parser");
 
 // db connection
 const db = require('./configs/db.config');
@@ -12,6 +13,7 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
--- server/routes/users.js
+++ server/routes/users.js
@@ -9,6 +9,7 @@ module.exports = (db) => {
     db.query(command)
       .then(data => {
         res.json(data.rows);
+        res.end();
       })
       .catch(err => err);
   });
@@ -19,13 +20,23 @@ module.exports = (db) => {
     db.query(command, [req.params.id])
       .then(data => {
         res.json(data.rows[0]);
+        res.end()
       })
       .catch(err => err);
   });
 
   /* POST create new user */
-  router.post('/', (req, res) => {
+  router.post('/', async (req, res) => {
+    // Hash and salt password
+    req.body.password = await bcrypt.hash(req.body.password, 10);
 
+    const params = Object.values(req.body);
+    const command = "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4);"
+    db.query(command, params)
+      .then(data => {
+        res.status(201).end();
+      })
+      .catch(err => err);
   });
 
   return router;
