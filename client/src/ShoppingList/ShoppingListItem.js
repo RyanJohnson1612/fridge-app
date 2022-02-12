f8d9d23b5cec89bc7fdd49d22f1fa4873362bda0
Bita <bita.janzadeh@hotmail.com>
2022-02-11 21:46:19 -0500

    Map through items in ShoppingListItem component, WIP

--- client/src/ShoppingList/ShoppingListItem.js
+++ client/src/ShoppingList/ShoppingListItem.js
@@ -7,7 +7,24 @@ function ShoppingListItem() {
     value: "",
   });
 
-  return <div></div>;
+  return items.map((item, index) => (
+    // class of item is assigned based on whether item was purchased or not
+    <div
+      className={item.isPurchased ? "item-row complete" : "item-row"}
+      key={index}
+    >
+      {/* keep track of which item is being clicked on */}
+      <div key={item.id} onClick={() => completeItem(item.id)}>
+        {item.text}
+      </div>
+    </div>
+  ));
 }
 
 export default ShoppingListItem;
+
+{
+  /* Function to check if item has been purchased or not */
+}
+
+Map through items in ShoppingListItem component, WIP
