a7d9a9cd82347009cfa019abc549a08e9c631db5
Justin Huang <justin.2260@gmail.com>
2022-02-12 13:11:41 -0500

    display info received from props onto page

--- client/src/components/FridgeItem/FridgeItem.jsx
+++ client/src/components/FridgeItem/FridgeItem.jsx
@@ -4,6 +4,15 @@ import './FridgeItem.scss';
 const FridgeItem = (props) => {
   return (
     <>
+      <img src={props.image} className='image' />
+      <div className='properties'>
+        <p>{props.name}</p>
+        <p>Category: {props.category}</p>
+        <p>Date Purchased: {props.dateStored}</p>
+        { props.expiry ? <p>Expiry Date: {props.expiry}</p> : "" }
+        { props.expiry ? <p>Time Remaining: {props.expiry - props.dateStored}</p> : ""}
+        <p>Notes: {props.notes}</p>
+      </div>
     </>
 
   );
