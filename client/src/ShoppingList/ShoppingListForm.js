0260faf873567fecac3b3b1c18fdd6b53424c5cf
Bita <bita.janzadeh@hotmail.com>
2022-02-11 19:26:02 -0500

    Prevent ShoppingList form from refreshing page on submit

--- client/src/ShoppingList/ShoppingListForm.js
+++ client/src/ShoppingList/ShoppingListForm.js
@@ -3,19 +3,22 @@ import React, { useState } from "react";
 function ShoppingListForm() {
   const [input, setInput] = useState("");
 
+  const handleSubmit = (e) => {
+    //Stop form refreshing page on submit
+    e.preventDefault();
+  };
+
   return (
-
-      <form className="shopping-form">
-        <input
-          type="text"
-          placeholder="Add a todo"
-          value={input}
-          name="text"
-          className="shopping-input"
-        />
-        <button className="todo-button"> Add todo</button>
-      </form>
-
+    <form className="shopping-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a todo"
+        value={input}
+        name="text"
+        className="shopping-input"
+      />
+      <button className="todo-button"> Add todo</button>
+    </form>
   );
 }
 
