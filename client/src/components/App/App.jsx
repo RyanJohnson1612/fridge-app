5a9e5c043060f801aea815f24d65ad2f5f07e807
Bita <bita.janzadeh@hotmail.com>
2022-02-12 15:24:09 -0500

    Implement responsive bootstrap navbar with styling

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav, Navbar } from "react-bootstrap";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 
 function App() {
   return (
     <div className="App">
-      Fridge App (Name WIP)
+      <Navbar bg="myTeal" variant="dark" fixed="top" expand="sm">
+        <Navbar.Brand href="/">FridgeApp</Navbar.Brand>
+        <Navbar.Toggle />
+        <NavbarCollapse>
+          <Nav>
+            <Nav.Link href="/"> MyFridge</Nav.Link>
+            <Nav.Link href="/"> Grocery List</Nav.Link>
+            <Nav.Link href="/"> Meal Ideas</Nav.Link>
+          </Nav>
+        </NavbarCollapse>
+      </Navbar>
+
+      <div className="content">Placeholder content...</div>
     </div>
   );
 }
