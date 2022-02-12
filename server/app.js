0bee0761f91b9697f3eba72dafe892e33aa1c04b
Justin Huang <justin.2260@gmail.com>
2022-02-12 13:06:34 -0500

    require cors and have express use cors

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors');
 
 // db connection
 const db = require('./configs/db.config');
@@ -11,6 +12,7 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
