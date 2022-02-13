2dec61d237ae51d03f2ca37035ec4d12d1b7b187
Bita <bita.janzadeh@hotmail.com>
2022-02-13 01:09:35 -0500

    Initial setup of Meal-ideas component

---
+++ client/src/components/Meal-Ideas/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MealIdeas() {
+  const APP_ID = "";
+  const APP_KEY = "";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  return (
+    <div>
+      <form>input</form>
+    </div>
+  );
+}
+
+export default MealIdeas;
