8e7a4489da18312c49fab9050910fc17eb281588
Bita <bita.janzadeh@hotmail.com>
2022-02-13 01:48:43 -0500

    Create input form on Meal-indeas component

--- client/src/components/Meal-Ideas/index.js
+++ client/src/components/Meal-Ideas/index.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function MealIdeas() {
-  const APP_ID = "";
-  const APP_KEY = "";
+  const APP_ID = "c91c9bd4";
+  const APP_KEY = "988029de8e29a4a1503fea286388dfef";
 
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Effect has been run");
+  }, []);
 
   return (
     <div>
-      <form>input</form>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          {counter}
+        </button>
+      </form>
     </div>
   );
 }
