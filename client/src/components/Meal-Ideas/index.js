9c202ed892b6aba245984394031ee0b54c2349b0
Bita <bita.janzadeh@hotmail.com>
2022-02-13 02:41:06 -0500

    Make axios request to test endpoint (edamam API) to get recipes data

--- client/src/components/Meal-Ideas/index.js
+++ client/src/components/Meal-Ideas/index.js
@@ -1,23 +1,35 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function MealIdeas() {
-  const APP_ID = "c91c9bd4";
-  const APP_KEY = "988029de8e29a4a1503fea286388dfef";
+  const APP_ID = "";
+  const APP_KEY = "";
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  const [counter, setCounter] = useState(0);
+  //const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   useEffect(() => {
-    console.log("Effect has been run");
+    getRecipes();
   }, []);
+
+  //Function responsible for getting recipe data from API
+  const getRecipes = () => {
+    axios
+      .get(
+        `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      )
+      .then((res) => {
+        console.log("recipes:", res.data.hits);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div>
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-button" type="submit">
-          {counter}
-        </button>
+        <button className="search-button" type="submit"></button>
       </form>
     </div>
   );
