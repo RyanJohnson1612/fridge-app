479486c209327eaaf8b6a7a9c562b894a96cc670
Justin Huang <justin.2260@gmail.com>
2022-02-13 01:37:55 -0500

    format text and image placement; add grocery list and trash icon

--- client/src/components/FridgeItem/FridgeItem.jsx
+++ client/src/components/FridgeItem/FridgeItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './FridgeItem.scss';
+import { BsCart4, BsTrash } from 'react-icons/bs';
 
 const FridgeItem = (props) => {
 
@@ -14,47 +15,60 @@ const FridgeItem = (props) => {
   };
 
   return (
-    <div className='body'>
-      <img src={props.image} className='image' />
-      <br />
-      <table className='properties'>
-        <tbody>
-          <tr>
-            <td>Food Item:</td>
-            <td>{props.name}</td>
-          </tr>
-          <tr>
-            <td>Category:</td>
-            <td>{props.category}</td>
-          </tr>
-          <tr>
-            <td>Date Purchased:</td>
-            <td>{props.dateStored}</td>
-          </tr>
-          <tr>
-            <td>Been in the Fridge since:</td>
-            <td>{daysAgo(props.storedSince)}</td>
-          </tr>
-          { props.expiry &&
+    <>
+      <div className='body'>
+        <img src={props.image} className='image' />
+        <br />
+        <table className='properties'>
+          <tbody>
             <tr>
-              <td>Expiry Date:</td>
-              <td>{props.expiry}</td>
+              <td width="40%">Food Item:</td>
+              <td><strong>{props.name}</strong></td>
             </tr>
-          }
-          { props.expiry &&
             <tr>
-              <td>Days Until Expiry:</td>
-              <td>{props.expireIn} Days</td>
+              <td>Category:</td>
+              <td><strong>{props.category}</strong></td>
             </tr>
-          }
-          <tr>
-            <td>Notes:</td>
-            <td>{props.notes}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+            <tr>
+              <td>Date Purchased:</td>
+              <td><strong>{props.dateStored}</strong></td>
+            </tr>
+            <tr>
+              <td>Been in the Fridge since:</td>
+              <td><strong>{daysAgo(props.storedSince)}</strong></td>
+            </tr>
+            { props.expiry &&
+              <tr>
+                <td>Expiry Date:</td>
+                <td><strong>{props.expiry}</strong></td>
+              </tr>
+            }
+            { props.expiry &&
+              <tr>
+                <td>Days Until Expiry:</td>
+                <td><strong>{props.expireIn} Days</strong></td>
+              </tr>
+            }
+            <tr>
+              <td>Notes:</td>
+              <td><strong>{props.notes}</strong></td>
+            </tr>
+          </tbody>
 
+        </table>
+      </div>
+      <br />
+      <div className='click-from-item'>
+        <div>
+          <p>Add to Grocery List?</p>
+          <BsCart4 size={40} />
+        </div>
+        <div>
+          <p>Clear item from Fridge?</p>
+          <BsTrash size={40} />
+        </div>
+      </div>
+    </>
   );
 }
 
