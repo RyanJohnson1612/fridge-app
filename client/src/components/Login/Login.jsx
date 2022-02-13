7397648dae0b8b05fe2bcc2e17bd9dee06b50562
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-12 22:50:31 -0500

    Create login submit handler

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,23 +1,26 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert('submit', email, password)
+  const handleSubmit = () => {
+    axios.post(`${process.env.REACT_APP_API_URL}/api/users/login`, {email, password})
+      .then(res => {
+        console.log(res);
+      })
   }
 
   return (
     <>
       <h1>Login to Fridge App</h1>
-      <form className="login__form">
+      <form className="login__form" onSubmit={e => e.preventDefault()}>
         <label className="login__label">Email</label>
         <input className="login__input" type="email" name="email" value={email} onChange={e => setEmail(e.currentTarget.value)}/>
-        <label lassName="login__label">Password</label>
+        <label className="login__label">Password</label>
         <input className="login__input" type="password" name="password" value={password} onChange={e => setPassword(e.currentTarget.value)}/>
-        <button type="submit" onSubmit={handleSubmit}>Login</button>
+        <button type="submit" onClick={() => handleSubmit()}>Login</button>
       </form>
     </>
   );
