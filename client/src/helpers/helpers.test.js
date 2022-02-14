34f0deb743ff881c996192e35a47dfa2249bd5c6
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-14 13:25:40 -0500

    Add tests for auth and getCookie functions

--- client/src/helpers/helpers.test.js
+++ client/src/helpers/helpers.test.js
@@ -1,1 +1,21 @@
-// Empty file so the folder gets tracked by git, remove once a file gets added to this folder
+import { auth, getCookie } from './helpers';
+
+describe('#auth', () => {
+  it('should return user object if logged in', () => {
+    document.cookie='user={"id": 1, "email": "jim@testman.com", "firstName": "Jim", "lastName": "Testman"}';
+    expect(auth()).toMatchObject({id: 1, email: 'jim@testman.com', firstName: 'Jim', lastName: 'Testman'});
+  });
+
+  it('should return null if not logged in', () => {
+    // Remove all cookies
+    document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+    expect(auth()).toBeNull();
+  });
+});
+
+describe('#getCookie', () => {
+  it('should return a cookie if it exists',() => {
+    document.cookie='test=this is a test cookie';
+    expect(getCookie('test')).toBe('this is a test cookie')
+  })
+})
