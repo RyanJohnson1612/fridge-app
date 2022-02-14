88c53ca30facfd4ec8898c9240484fb46d2bb3cb
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-14 13:25:58 -0500

    Create auth and getCookie functions

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -1,1 +1,15 @@
-// Empty file so the folder gets tracked by git, remove once a file gets added to this folder
+const getCookie = (name) => {
+  var cookies = '; ' + document.cookie;
+  var splitCookie = cookies.split('; ' + name + '=');
+  if (splitCookie.length === 2) return splitCookie.pop();
+}
+
+module.exports.getCookie = getCookie;
+
+const auth = () => {
+  const user = getCookie('user');
+  if(!user) return null;
+  return JSON.parse(user);
+}
+
+module.exports.auth = auth;
