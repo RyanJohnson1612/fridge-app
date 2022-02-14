3a995236fc62d98a30b8a4898e81e8469e5955dc
Bita <bita.janzadeh@hotmail.com>
2022-02-14 17:27:34 -0500

    Refactor Meal-Ideas/index.js

--- client/src/components/Meal-Ideas/index.js
+++ client/src/components/Meal-Ideas/index.js
@@ -12,30 +12,27 @@ function MealIdeas() {
     "Gluten-Free",
     "Pork-Free",
   ];
+
   const APP_ID = "c91c9bd4";
   const APP_KEY = "988029de8e29a4a1503fea286388dfef";
 
-  //State to manage multiple checkboxes used for recipe filters
+  //State to manage multiple checkboxes used for dietRestrictions recipe filters
   const [checkedState, setCheckedState] = useState(
     new Array(dietRestrictions.length).fill(false)
   );
-
-  const [filters, setFilters] = useState("");
-
-  console.log("checkedState", checkedState);
+  const [filters, setFilters] = useState([]);
 
   /////NOTE: UNCOMMENT if want to use searchbar functionality
   //state will be set to data that comes back from edamam API
   const [recipes, setRecipes] = useState([]);
   //const [search, setSearch] = useState("");
 
-  //Use to determine if loading spinner should be displayed
+  //State to determine if loading spinner should be displayed
   const [loading, setLoading] = useState(true);
 
   /* If useEffect were to run everytime search state is updated,
   there would be be an API call for every keystroke.
   Therefore, create query state which only updates after search button is clicked */
-
   /////NOTE: UNCOMMENT if want to use searchbar functionality, and place "query" in array
   //const [query, setQuery] = useState("");
 
@@ -43,41 +40,31 @@ function MealIdeas() {
     getRecipes();
   }, [filters]);
 
+  //Updates filters state based on dietaryRestrictions checked off
   const handleOnChange = (position) => {
+    const allFilters = [];
     const updatedCheckedState = checkedState.map((item, index) =>
       index === position ? !item : item
     );
-
     setCheckedState(updatedCheckedState);
 
-    const allFilters = updatedCheckedState.reduce(
-      (combined, currentState, index) => {
-        if (currentState === true) {
-          return combined + " " + dietRestrictions[index];
-        }
-        return combined;
-      },
-      ""
-    );
-
-    setFilters(allFilters);
-    console.log("these are the filters you selected:", filters);
+    updatedCheckedState.forEach((currentState, index) => {
+      if (currentState === true) {
+        allFilters.push(dietRestrictions[index]);
+      }
+    });
+    setFilters([...allFilters]);
   };
 
   const healthLabels = () => {
-    //Convert string of filters to an array
     let result = "";
-    if (filters !== "") {
-      let filtersArray = filters.slice(1).split(" ");
-      filtersArray.forEach((filter) => {
+    if (filters.length > 0) {
+      filters.forEach((filter) => {
         result += `&health=${filter}`;
       });
     }
     return result.toLowerCase();
   };
-
-  const healthLabelz = healthLabels();
-  console.log("THESE ARE THE HEALTH LABELS:", healthLabelz);
 
   //Temporarily hardcode, in future pull from DB
   const expiringFoodItems = "Peas";
@@ -87,15 +74,10 @@ function MealIdeas() {
   const getRecipes = () => {
     axios
       .get(
-        `https://api.edamam.com/search?q=${expiringFoodItems}&app_id=${APP_ID}&app_key=${APP_KEY}${healthLabelz}`
+        `https://api.edamam.com/search?q=${expiringFoodItems}&app_id=${APP_ID}&app_key=${APP_KEY}${healthLabels()}`
       )
       .then((res) => {
         setRecipes(res.data.hits);
-        console.log(res.data.hits);
-        console.log(
-          "AXIOS GET ROUTE:",
-          `https://api.edamam.com/search?q=${expiringFoodItems}&app_id=${APP_ID}&app_key=${APP_KEY}&health=Vegan&health=Pork-Free`
-        );
         setLoading(false);
       })
       .catch((err) => {
@@ -150,7 +132,6 @@ function MealIdeas() {
           );
         })}
       </div>
-      <h1>{healthLabelz}</h1>
       <div className="recipes-list">
         {recipes.map((recipe) => (
           <Recipe
