9e054ef29e5a72ea244a713f53082a9b69039355
Bita <bita.janzadeh@hotmail.com>
2022-02-13 22:10:40 -0500

    Change variable name and code-cleanup in Recipe.js for clarity

--- client/src/components/Meal-Ideas/Recipe.js
+++ client/src/components/Meal-Ideas/Recipe.js
@@ -6,23 +6,18 @@ function Recipe(props) {
   const { title, image, recipeURL, ingredients } = props;
   const [open, setOpen] = useState(false);
 
-  //If recipe has more than 5 ingredients, insert a collapsable component that must be opened to see additional ingredients
+  /*   When recipe has 5+ ingredients, list first 5, then insert a collapsable
+  component that must be opened to see additional ingredients */
   const ingredientsFormatter = () => {
     let result = [];
     const ingredientsArray = ingredients.map((ingredient) => (
       <li> {ingredient.text} </li>
     ));
-
     if (ingredientsArray.length > 5) {
       result = ingredientsArray.slice(0, 5);
       result.push(
         <div>
-          <Button
-            variant="link"
-            onClick={() => setOpen(!open)}
-            aria-expanded={open}
-            size="sm"
-          >
+          <Button variant="link" onClick={() => setOpen(!open)} size="sm">
             ...See More
           </Button>
           <Collapse in={open}>
@@ -34,19 +29,17 @@ function Recipe(props) {
     } else {
       result = ingredientsArray;
     }
-
     return result;
   };
 
-  const ingredientsArray = ingredientsFormatter();
-  console.log("ingredients length", ingredientsArray.length);
+  const ingredientsMapped = ingredientsFormatter();
 
   return (
     <div className="Recipe">
       <h4> {title} </h4>
       <ol>
         <h6> Ingredients: </h6>
-        {ingredientsArray}
+        {ingredientsMapped}
       </ol>
       <div className="recipe-card-footer">
         <img src={image} alt="recipe-image" />
