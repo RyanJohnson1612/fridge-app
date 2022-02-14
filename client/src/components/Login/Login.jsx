b694af501a59ed82d564596e3fce1ee8748b544d
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-14 16:58:54 -0500

    Update error styling and logic for login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -5,51 +5,54 @@ import { useNavigate } from 'react-router-dom';
 import { auth } from '../../helpers/helpers';
 axios.defaults.withCredentials = true;
 
-function Login() {
+function Login(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [errors, setErrors] = useState({});
   let navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if(auth()) return navigate('/fridge');
-  // },[navigate])
-
   const validateForm = () => {
-    setError('')
+    setErrors({})
+
     if(!email) {
-      setError('Email is required');
-      return;
+      setErrors(prev => ({...prev, email: 'Email is required'}));
     }
     if(!password) {
-      setError('Password is required');
-      return;
+      setErrors(prev => ({...prev, password: 'Password is required'}));
     }
 
-    submitForm();
+    if(Object.keys(errors).length === 0) return true;
+
+    return false
   }
 
-  const submitForm = () => {
-    axios.post(`${process.env.REACT_APP_API_URL}/api/users/login`, {email, password})
-      .then(res => {
-        if(auth()) return navigate('/fridge');
-      })
-      .catch(err => {
-        if (err.responsew) {
-          setError(err.response.data.error)
-        }
-        console.log(err);
-      })
+  const submitForm = async () => {
+    const valid = await validateForm();
+
+    if(valid) {
+      axios.post(`${process.env.REACT_APP_API_URL}/api/users/login`, {email, password})
+        .then(res => {
+          if(auth()) return navigate('/fridge');
+        })
+        .catch(err => {
+          if (err.response) {
+            setErrors(prev => ({...prev, server: err.response.data.error}));
+          }
+          console.log(err);
+        })
+    }
   }
+
+  const parsedErrors = [...new Set(Object.values(errors))].map((error, index) => <li className="register__error_message" key={index}>{error}</li>);
 
   return (
     <>
       <h1>Login to Fridge App</h1>
       <Form className="login__form" onSubmit={e => e.preventDefault()}>
         <Form.Group>
-          <Form.Label className="login__label">Email</Form.Label>
+          <Form.Label className={errors.email ? 'login__label login__label--error' : 'login__label'}>Email</Form.Label>
           <Form.Control
-            className="login__input"
+            className={errors.email ? 'login__input login__input--error' : 'login__input'}
             type="email"
             value={email}
             onChange={e => setEmail(e.currentTarget.value)}
@@ -57,16 +60,24 @@ function Login() {
         </Form.Group>
 
         <Form.Group>
-          <Form.Label className="login__label">Password</Form.Label>
+          <Form.Label className={errors.password ? 'login__label login__label--error' : 'login__label'}>Password</Form.Label>
           <Form.Control
-            className="login__input"
+            className={errors.password ? 'register__input register__input--error' : 'register__input'}
             type="password"
             value={password}
             onChange={e => setPassword(e.currentTarget.value)}
           />
         </Form.Group>
-        <Button className="login__button" type="submit" onClick={() => validateForm()}>Login</Button>
-        { error && <Alert variant={'danger'}>{error}</Alert> }
+        <Button className="login__button" type="submit" onClick={() => submitForm()}>Login</Button>
+
+        {
+          Object.keys(errors).length > 0 &&
+          <Alert className="login__error" variant={'danger'}>
+            <ul className="login__error_list">
+              {parsedErrors}
+            </ul>
+          </Alert>
+        }
       </Form>
     </>
   );
