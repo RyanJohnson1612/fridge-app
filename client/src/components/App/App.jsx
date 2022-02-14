facba3dead862fce0ddb30158e8241c37805bc94
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-14 17:50:00 -0500

    Moved navbar into separate component

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,9 +1,7 @@
-import React from "react";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Nav, Navbar } from "react-bootstrap";
-import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from '../Navbar/'
 import ShoppingList from '../ShoppingList/';
 import Login from '../Login/';
 import Register from '../Register/';
@@ -11,21 +9,8 @@ import Register from '../Register/';
 function App() {
   return (
     <main className="App">
-        <Router>
-        <Navbar bg="myTeal" variant="dark" fixed="top" expand="sm">
-          <Navbar.Brand href="/">FridgeApp</Navbar.Brand>
-          <Navbar.Toggle />
-          <NavbarCollapse>
-            <Nav>
-              <Nav.Link as={Link} to="/fridge">MyFridge</Nav.Link>
-              <Nav.Link as={Link} to="/grocery-list">Grocery List</Nav.Link>
-              <Nav.Link as={Link} to="/recipes">Recipe Ideas</Nav.Link>
-              <Nav.Link as={Link} to="/login">Login</Nav.Link>
-              <Nav.Link as={Link} to="/register">Register</Nav.Link>
-            </Nav>
-          </NavbarCollapse>
-        </Navbar>
-
+      <Router>
+        <Navbar />
         <section className="content">
           <Routes>
             <Route path="/fridge" element={<h1>Fridge Index</h1>} />
