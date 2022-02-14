27bbb8c5725cbf9059e84c432875010e7b0050bd
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-14 12:18:49 -0500

    Install jwtwebtoken and cookie parser as dependencies

--- server/app.js
+++ server/app.js
@@ -3,9 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const bodyParser = require("body-parser");
-const cors = require('cors');
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 // db connection
 const db = require('./configs/db.config');
 
@@ -13,9 +12,17 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
