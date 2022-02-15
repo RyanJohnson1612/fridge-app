73a9f90ca6fcf4fead5e858f0207ace764f6c3a6
Bita <bita.janzadeh@hotmail.com>
2022-02-14 23:52:13 -0500

    Save data from axios call in testAxiosGet Promise

--- client/src/components/ShoppingList/ShoppingListItem.js
+++ client/src/components/ShoppingList/ShoppingListItem.js
@@ -3,6 +3,7 @@ import ShoppingListForm from "./ShoppingListForm";
 import { RiCloseCircleLine, RiFridgeLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
 import axios from "axios";
+
 function ShoppingListItem(props) {
   const { items, completeItem, removeItem, updateItem, addToFridge } = props;
 
@@ -19,21 +20,19 @@ function ShoppingListItem(props) {
     });
   };
 
-  const testAxiosGet = () => {
-    axios
-      .get(`http://localhost:8080/grocery_lists/3`)
-      .then((res) => {
-        res.data.forEach((data, index) => {
-          console.log("index", index);
-        });
-        console.log("Get grocery list 3:", res.data);
-      })
-      .catch((err) => {
-        console.log(err);
+  const testAxiosGet = axios
+    .get(`http://localhost:8080/grocery_lists/3`)
+    .then((res) => {
+      const result = [];
+      res.data.forEach((data) => {
+        result.push(data.name);
       });
-  };
-
-  testAxiosGet();
+      return result;
+    })
+    .then({})
+    .catch((err) => {
+      console.log(err);
+    });
 
   /*  If state variable edit.id is true (which occurs after edit icon is clicked),
   return ShoppingListForm while passing submitUpdate function onSubmit */
