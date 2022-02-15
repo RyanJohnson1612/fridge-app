bf113a1731b1b7b83548c7e334241e0933b9511a
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-15 15:41:10 -0500

    Fix protectedRoute middleware

--- middleware/protectedRoute.js
+++ middleware/protectedRoute.js
@@ -1,23 +1,31 @@
 const { verify } = require('jsonwebtoken');
+const { promisify } = require('util');
+const db = require('../configs/db.config');
+
+/* Decrypts jwt  */
+const decryptToken = async (token) => {
+  //convert jwt.verify to a function that returns promise
+  const verifyToken = promisify(verify);
+  return await verifyToken(token, process.env.JWT_SECRET);
+}
 
 /* Route protection middleware, requires a jsonwebtoken (access-token) to be set to access route */
-
-const protectedRoute = (req, res, next) => {
-  console.log(req.cookies)
+const protectedRoute = async (req, res, next) => {
   const accessToken = req.cookies['access-token'];
 
   if (!accessToken) {
     return res.status(401).send('Unauthorized')
   }
 
-  try {
-    console.log('valid', valid)
-    if(verify(accessToken, process.env.JWT_SECRET)) {
-      req.authenticated = true;
-      return next();
-    }
-  } catch (err) {
-    return res.status(400).json({error: err})
+  const decryptedToken = await decryptToken(accessToken);
+
+  if(decryptedToken) {
+    req.user = { id: decryptedToken.id, email: decryptedToken.email };
+    req.authenticated = true;
+    return next();
+
+  } else {
+    return res.status(400).json('Error authorizing')
   }
 }
 
