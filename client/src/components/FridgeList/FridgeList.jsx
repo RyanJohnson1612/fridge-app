71c33a51025cbf97d92cd098d79a46508d661eb2
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-15 23:38:18 -0500

    Create FridgeList component

---
+++ client/src/components/FridgeList/FridgeList.jsx
@@ -0,0 +1,17 @@
+import FridgeCard from "../FridgeCard/";
+
+
+
+function FridgeList(props) {
+  const parsedItems = props.items.map((item) => <FridgeCard item={item} key={item.id}/>);
+
+  return (
+    <>
+      <div className="fridge-list">
+        {parsedItems}
+      </div>
+    </>
+  )
+}
+
+export default FridgeList
