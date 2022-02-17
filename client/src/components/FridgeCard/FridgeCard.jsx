00e577b5c34e64c12581c02207d1dcdcd76c1dc2
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-17 00:44:00 -0500

    Remove unused code from FridgeCard

--- client/src/components/FridgeCard/FridgeCard.jsx
+++ client/src/components/FridgeCard/FridgeCard.jsx
@@ -6,7 +6,7 @@ import moment from 'moment';
 function FridgeCard(props) {
   const daysUntilExpired = moment(props.item.expiry).endOf('day').diff(moment().endOf('day'), 'days');
 
-  const status = (daysLeft) => {
+  const status = () => {
     if (daysUntilExpired > 3) {
       return 'Fresh';
     } else if (daysUntilExpired <= 3 && daysUntilExpired > 0) {
@@ -19,9 +19,9 @@ function FridgeCard(props) {
   }
 
   const statusClassName = classNames('fridge-card__status', {
-    'fridge-card__status--fresh': status(daysUntilExpired) === 'Fresh',
-    'fridge-card__status--expiring': status(daysUntilExpired) === 'Expiring Soon',
-    'fridge-card__status--expired': status(daysUntilExpired) === 'Expired',
+    'fridge-card__status--fresh': status() === 'Fresh',
+    'fridge-card__status--expiring': status() === 'Expiring Soon',
+    'fridge-card__status--expired': status() === 'Expired',
   })
 
   return (
@@ -29,14 +29,10 @@ function FridgeCard(props) {
       <Link to={`/fridge-items/${props.item.id}`}>
         <div className="fridge-card__header">
           <img src={props.item.image_url} alt={props.item.name} className="fridge-card__image"/>
-          <div className={statusClassName}>{status(daysUntilExpired)}</div>
+          <div className={statusClassName}>{status()}</div>
         </div>
         <div className="fridge-card__footer">
           <h4 className="fridge-card__name">{props.item.name}</h4>
-          <h5 className="fridge-card__date">
-            days until expired {daysUntilExpired}
-            Category: {props.item.category}
-          </h5>
           <h5 className="fridge-card__date">
             <BsFillClockFill />
             <span>Added {moment(props.item.date_stored).fromNow()}</span>
