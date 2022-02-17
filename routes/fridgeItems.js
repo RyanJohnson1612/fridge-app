c7c3d38a5a0f8402593b79897bbc3ccecefb0e89
Justin Huang <justin.2260@gmail.com>
2022-02-16 22:39:47 -0500

    update query for all firdge_items

--- routes/fridgeItems.js
+++ routes/fridgeItems.js
@@ -4,8 +4,14 @@ module.exports = (db) => {
 
   // GET food listing
   router.get('/', function(req, res, next) {
-    const command = "SELECT * FROM fridge_items";
-    db.query(command).then(data => {
+    const queryString =
+      `SELECT id, name, category, image_URL, notes, date_removed,
+      to_char(date_stored, 'Mon DD, YYYY') as date_stored,
+      to_char(expiry, 'Mon DD, YYYY') as expiry,
+      (expiry - date_stored) as expire_in,
+      (CURRENT_DATE - date_stored) as stored_since
+      FROM fridge_items`;
+    db.query(queryString).then(data => {
       console.log(data.rows)
       res.json(data.rows);
     }).catch(error => console.log(`Error: ${error.message}`));
