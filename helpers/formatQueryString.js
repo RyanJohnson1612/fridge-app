5a16b4c21c09150f0098a3ee4379875225c7056a
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-18 16:58:31 -0500

    Add but then also removed days until expires filter, it doesn't really work with the status filter

--- helpers/formatQueryString.js
+++ helpers/formatQueryString.js
@@ -1,16 +1,23 @@
+const moment = require('moment');
+
 const formatQueryString = (params) => {
   if (!params) return '';
 
   return Object.keys(params).reduce((prev, current) => {
-    if(current !== 'search') {
+    console.log(current)
+    if(current !== 'search' && current !== 'expiresIn') {
       if (params[current].includes(',')) {
         prev += ` AND (${current} = '${params[current].replace(/,/g, `' OR ${current} = '`)}')`;
       } else {
         prev += ` AND (${current} = '${params[current]}')`;
       }
-    } else {
+    } else if (current === 'search'){
       prev += ` AND (UPPER(${current}) LIKE UPPER('%${params[current].trim()}%'))`;
     }
+    // else if (current === 'expiresIn') {
+    //   const days = params[current].split(',')
+    //   prev += ` AND (expiry BETWEEN '${moment().add(Number(days[0]), 'days').format('YYYY-MM-DD')}' AND '${moment().add(Number(days[1]), 'days').format('YYYY-MM-DD')}')`
+    // }
       return prev;
 
   }, '');
