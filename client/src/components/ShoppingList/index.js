960bc6b78f030d6f7366f2991bb13a4f0f98812c
Bita <bita.janzadeh@hotmail.com>
2022-02-18 18:22:40 -0500

    Include user object in shopping list component

--- client/src/components/ShoppingList/index.js
+++ client/src/components/ShoppingList/index.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import ShoppingList from "./ShoppingList";
 import "./ShoppingList.scss";
+import { authContext } from "../../providers/AuthProvider";
 
 function ShoppingListIndex() {
+  const { user } = useContext(authContext);
+  console.log(user);
   return (
     <div className="shopping-list">
       <ShoppingList />
