559759e298531160a61363024e42257350689b4b
Bita <bita.janzadeh@hotmail.com>
2022-02-19 18:15:30 -0500

    Fix typo

--- client/src/components/Navbar/NewGroceryListModal.js
+++ client/src/components/Navbar/NewGroceryListModal.js
@@ -1,18 +1,27 @@
 import { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
 import axios from "axios";
+import { useNavigate } from 'react-router-dom'
+
 
 function NewGroceryListModal() {
 
   //States responsible for modal display
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
   const [show, setShow] = useState(false);
+
   const [input, setInput] = useState('');
 
-  const handleSubmit = (e) => {
+  const navigate = useNavigate();
 
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setInput(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
     axios
     .post(`${process.env.REACT_APP_API_URL}/grocery_lists`, {
       user_id: 2,
@@ -20,19 +29,15 @@ function NewGroceryListModal() {
     })
     .then((res) => {
       console.log("New grocery list submitted!", res.data)
+      navigate(`/grocery-list/${res.data.id}`)
     })
     .catch((err) => {
       console.log("Grocery list submssion error", err);
     });
 
-    setInput("");
     handleClose();
   }
 
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setInput(e.target.value)
-  }
 
   return (
     <>
