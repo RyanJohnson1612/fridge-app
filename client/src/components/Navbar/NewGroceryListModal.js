beb9214fdcd9e12ce88a8d2bea39888a39894e67
Bita <bita.janzadeh@hotmail.com>
2022-02-20 11:29:03 -0500

    New grocery list form submits by pressing enter key

--- client/src/components/Navbar/NewGroceryListModal.js
+++ client/src/components/Navbar/NewGroceryListModal.js
@@ -1,44 +1,41 @@
 import { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
-
-function NewGroceryListModal({setAllGroceryLists, allGroceryLists}) {
-
+function NewGroceryListModal({ setAllGroceryLists, allGroceryLists }) {
   //States responsible for modal display
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const [show, setShow] = useState(false);
 
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     console.log(e.target.value);
-    setInput(e.target.value)
-  }
+    setInput(e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-    .post(`${process.env.REACT_APP_API_URL}/grocery_lists`, {
-      user_id: 2,
-      name: input,
-    })
-    .then((res) => {
-      console.log("New grocery list submitted!", res.data)
-      setAllGroceryLists([...allGroceryLists, res.data])
-      navigate(`/grocery-list/${res.data.id}`)
-    })
-    .catch((err) => {
-      console.log("Grocery list submssion error", err);
-    });
+      .post(`${process.env.REACT_APP_API_URL}/grocery_lists`, {
+        user_id: 2,
+        name: input,
+      })
+      .then((res) => {
+        console.log("New grocery list submitted!", res.data);
+        setAllGroceryLists([...allGroceryLists, res.data]);
+        navigate(`/grocery-list/${res.data.id}`);
+      })
+      .catch((err) => {
+        console.log("Grocery list submssion error", err);
+      });
 
     handleClose();
-  }
-
+  };
 
   return (
     <>
@@ -51,13 +48,21 @@ function NewGroceryListModal({setAllGroceryLists, allGroceryLists}) {
           <Modal.Title>New Grocery List</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Grocery List Name</Form.Label>
-              <Form.Control type="text" placeholder="Name your new Grocery List" onChange={handleChange} />
+              <Form.Control
+                type="text"
+                placeholder="Name your new Grocery List"
+                onChange={handleChange}
+                autofocus
+              />
             </Form.Group>
-            <Button variant="primary" type="button" onClick={(e) => handleSubmit(e)}>
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={(e) => handleSubmit(e)}
+            >
               Submit
             </Button>
           </Form>
