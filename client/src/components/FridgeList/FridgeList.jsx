a8eccda9b7abf534302ce13fcd422a9414795490
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-20 15:45:32 -0500

    Redirect user to recipe ideas with search params, when click find recipe ideas button

--- client/src/components/FridgeList/FridgeList.jsx
+++ client/src/components/FridgeList/FridgeList.jsx
@@ -1,15 +1,23 @@
 import FridgeCard from '../FridgeCard/';
+import { useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap'
 import useCheckList from '../../hooks/useCheckList/useCheckList';
 
 function FridgeList(props) {
   const { selected, handleCheck } = useCheckList();
+  const navigate = useNavigate();
+
+  const redirectToRecipes = () => {
+    const query = `?q=${selected.join(',')}`;
+    navigate(`/recipes${query}`);
+  };
 
   const parsedItems = props.items.map((item) => <FridgeCard item={item} key={item.id} onChecked={handleCheck} checkboxVisible={props.checkboxVisible} />);
 
   return (
     <>
-      <Button onClick={() => props.selectIngredients()}>{props.checkboxVisible ? 'Cancel' : 'Select recipe ingredients'}</Button>
+      {props.checkboxVisible && <Button onClick={() => redirectToRecipes()}>Find Recipe Ideas</Button>}
+      <Button onClick={() => props.selectIngredients()} variant={props.checkboxVisible ? 'danger' : 'primary'}>{props.checkboxVisible ? 'Cancel' : 'Select recipe ingredients'}</Button>
       <div className="fridge-list">
         {parsedItems.length > 0 ? parsedItems : <h3>No results found</h3>}
       </div>
