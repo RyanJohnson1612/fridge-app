400dd74a0bfcf9a5a5848ecc670061623326e65a
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-20 01:19:22 -0500

    Refactor some of CheckList logic to useList hook

---
+++ client/src/hooks/useList/useList.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+function useCheckList() {
+  const [selected, setSelected] = useState([]);
+
+  const handleCheck = (option) => {
+    console.log('handle deez nuts')
+    if (selected.includes(option)) {
+      const activeArr = [...selected];
+      const index = activeArr.indexOf(option);
+      activeArr.splice(index, 1);
+      setSelected(activeArr);
+    } else {
+      setSelected(prev => ([...prev, option]));
+    }
+  }
+
+  return {selected, setSelected, handleCheck}
+}
+
+export default useCheckList;
