74a6e4fcfc46ce10dfb9e74fb887f02cefce2a6e
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-21 11:50:25 -0500

    Display prediction results from api request

--- client/src/components/Camera/Camera.jsx
+++ client/src/components/Camera/Camera.jsx
@@ -1,11 +1,13 @@
-import {  useRef } from 'react';
-import { BsFillCameraFill } from 'react-icons/bs'
+import {  useRef, useState } from 'react';
+import { BsFillCameraFill } from 'react-icons/bs';
+import { Button } from 'react-bootstrap';
 import useCamera from '../../hooks/useCamera/useCamera';
 import axios from 'axios';
 import './Camera.scss';
 import FormData from 'form-data';
 
 function Camera(props) {
+  const [predictions, setPredictions] = useState([]);
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
 
@@ -21,7 +23,7 @@ function Camera(props) {
     const video = document.getElementsByClassName("camera__video")[0];
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
     canvas.toBlob(img => {
-      let  data = new FormData();
+      let data = new FormData();
       data.append('file', img, 'img.png');
       axios
         .post(`${process.env.REACT_APP_API_URL}/api/images`, data, { headers: {
@@ -30,14 +32,16 @@ function Camera(props) {
         .then(res => {
           console.log(res);
           context.clearRect(0, 0, canvas.width, canvas.height);
-          document.getElementById('new-image').setAttribute('src', res.data.Location);
-
+          document.getElementById('new-image').setAttribute('src', res.data.image);
+          setPredictions(res.data.predictions);
         })
         .catch(err => {
           console.log(err);
         })
     })
   }
+
+  const parsedPredictions = predictions.map(prediction => <li>{prediction.name} - {prediction.value}</li>)
 
   return (
     <>
@@ -49,7 +53,13 @@ function Camera(props) {
         </button>
       </div>
 
-      <img id="new-image" alt="Coolio"/>
+      <ul className="prediction-list">
+        {parsedPredictions}
+      </ul>
+      {/* <input type="file" accept="image/*" capture="camera" name="file" onChange={(e) => handleChange(e)}/> */}
+
+
+      <img id="new-image"/>
     </>
   );
 }
