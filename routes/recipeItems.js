27d4407b966025c03cb46f46be2bb33b2196d0e7
Bita <bita.janzadeh@hotmail.com>
2022-02-21 17:02:00 -0500

    Modify query for GET recipeItems to be user specific

--- routes/recipeItems.js
+++ routes/recipeItems.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const createToken = require('../helpers/createToken');
+const protectedRoute = require('../middleware/protectedRoute');
 
 module.exports = (db) => {
 
-  /* GET list 3 items in fridge closest to expiring */
+  /* GET list 3 items in fridge closest to expiring for SPECIFIC user*, to use in default recipe recommendation*/
   //user_id temporarily hardcoded
-  router.get('/', function(req, res, next) {
+  router.get('/', protectedRoute, function(req, res, next) {
     const queryString =
       `SELECT fridge_items.id, fridge_items.name,
-      (expiry - date_stored) as expire_in
       FROM fridge_items
       JOIN fridges ON fridges.id = fridge_id
-      WHERE (expiry - date_stored)  > 0
+      WHERE (expiry - date_stored) > 0
       AND (date_removed IS NULL)
-      AND user_id = 1
+      AND user_id = $1
       ORDER BY (expiry - date_stored)
-      LIMIT 3;`  ;
-    db.query(queryString).then(data => {
+      LIMIT 3`;
+
+    const queryParams = [req.user.id]
+    db.query(queryString, queryParams).then(data => {
       console.log(data.rows)
       res.json(data.rows);
     }).catch(error => console.log(`Error: ${error.message}`));
