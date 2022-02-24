2c51ed0ef153815d37c8194898127cdfb25094eb
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-24 13:18:56 -0500

    Update pg connection to use connectionString

--- configs/db.config.js
+++ configs/db.config.js
@@ -4,12 +4,9 @@ pg.defaults.ssl = true;
 
 const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT, TEST_DATABASE} = process.env;
 
+
 const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-  database: process.env.NODE_ENV !== 'test' ? DB_DATABASE : TEST_DATABASE,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
