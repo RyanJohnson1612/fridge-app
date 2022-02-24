65029d8d23dc42a196dc8525ad18ba76cd0995f4
RyanJohnson1612 <ryanjohnson@rogers.com>
2022-02-24 13:25:45 -0500

    Change connection type from pool to client

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,19 +1,19 @@
 // Database connections
-const { Pool } = require('pg');
+const { Client } = require('pg');
 pg.defaults.ssl = true;
 
 const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT, TEST_DATABASE} = process.env;
 
 
-const pool = new Pool({
+const client = new Client({
   connectionString: process.env.DATABASE_URL + '?sslmode=require',
   ssl: { rejectUnauthorized: false }
 });
 
-pool.connect().then(() => {
+client.connect().then(() => {
 	console.log("Database connection established.")
 }).catch( e => {
 	throw new Error(e);
 })
 
-module.exports = pool;
+module.exports = client;
